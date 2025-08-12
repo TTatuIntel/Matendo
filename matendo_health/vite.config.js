16ea098de3869149f5488c798b306eca64773e4f
Kraiba Semakula <krabz01@gmail.com>
2025-08-12 13:07:18 +0300

    Update build configuration to use dist directory

--- matendo_health/vite.config.js
+++ matendo_health/vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
         }),
     ],
     build: {
-        outDir: 'public/build',
+        outDir: 'dist',
         assetsDir: '',
-        manifest: true,
+        manifest: true
     }
 });
