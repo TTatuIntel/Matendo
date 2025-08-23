df644b5985c0a70355d2e3d5803df5e992784186
Kraiba Semakula <krabz01@gmail.com>
2025-08-23 21:32:52 +0300

    chore: update app URLs and login links for production domain matendohealth.com

--- matendo_health/app/Providers/AppServiceProvider.php
+++ matendo_health/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+            // Optional, ensures helpers use the correct root:
+            URL::forceRootUrl(config('app.url'));
+        }
     }
 }
